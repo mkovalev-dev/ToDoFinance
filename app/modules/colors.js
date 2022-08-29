671415958de30d8d664e59409ba9ef807a9a8be5
maximkovalev <mkovalevhse@yandex.ru>
2022-08-29 21:18:09 +0300

    default colors and styles

---
+++ app/modules/colors.js
@@ -0,0 +1,42 @@
+export const COLORS_PRIMARY = {
+    DEFAULT: "#0772FF",
+    DEFAULT_STRONG: "#07499F",
+    BACKGROUND_LIGHT: "#F4F9FF",
+}
+
+export const COLORS_GRAYSCALE = {
+    HEADER: "#14142B",
+    HEADER_WEAK: "#262338",
+    BODY: "#4E4B66",
+    LABEL: "#6E7191",
+    PLACEHOLDER: "#7D87AC",
+    LINE: "#D9DBE9",
+    INPUT: "#EFF0F6",
+    WHITE_WEAK:"#F7F7FC",
+    WHITE:"#FCFCFC",
+}
+
+export const ERROR = {
+    DEFAULT: "#ca0227",
+    FLAT:"#e53e3e"
+}
+
+export const SUCCESS = {
+    DEFAULT: "#008A00",
+    FLAT:"#4eb04e"
+}
+
+export const WARNING = {
+    DEFAULT: "#EAAC30",
+    FLAT:"#ecb664"
+}
+
+export const PRIMARY_GRADIENT = {
+    START: "#0772FF",
+    END:"#5EA4FF"
+}
+
+export const SECONDARY_GRADIENT = {
+    START: "#262338",
+    END:"#403F49"
+}
